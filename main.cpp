2c52beb5a7d2e2261b8988117b7fb285af5c29b1
KakeruUeda <kakecoco2580@icloud.com>
2025-03-29 16:49:44 +0900

    create Gmae class

--- main.cpp
+++ main.cpp
@@ -1,22 +1,114 @@
 #include <X11/Xlib.h>
 #include <cstdio>
+#include <stdexcept>
 
-int main() {
+#define KEY_ESCAPE     61
+#define KEY_SPACEBAR   57
+#define KEY_UP        134
+#define KEY_DOWN      133
+#define KEY_LEFT      131
+#define KEY_RIGHT     132
 
-    Display *d = XOpenDisplay(NULL);
-    if(d == NULL) {
-        printf("Unable to open the display\n");
-        return -1;
+namespace mygame {
+
+class GameDisplay {
+public:
+    GameDisplay();
+    ~GameDisplay();
+
+    Display* getDisplay();
+
+private:
+    Display *display_;
+    int screen_;
+    Window window_;
+};
+
+// RAII -- Resource Acquisitio Is Initialization
+GameDisplay::GameDisplay() {
+    display_ = XOpenDisplay(NULL);
+    if(display_ == NULL) {
+        throw std::runtime_error("Unable to open the display\n");
+    }
+    
+    screen_ = DefaultScreen(display_);
+    
+    window_ = XCreateSimpleWindow(display_, RootWindow(display_, screen_), 0, 0, 100, 100, 1, 
+                                    BlackPixel(display_, screen_), WhitePixel(display_, screen_));
+    
+    
+    XSelectInput(display_, window_, KeyPressMask);
+    XMapWindow(display_, window_);
+}
+
+GameDisplay::~GameDisplay() {
+    XCloseDisplay(display_);
+}
+
+Display *GameDisplay::getDisplay() {
+    return display_;
+}
+
+class Game {
+public:
+    Game();
+
+    void run();
+
+private:
+    GameDisplay gamedisplay_;
+    XEvent event_;
+
+    bool is_running_ = true; 
+
+    bool getEvent();
+    void handleEvent();
+};
+
+Game::Game() {
+}
+
+void Game::run() {
+    while (is_running_) {
+        if (getEvent()){
+            handleEvent();
+        };
+    }
+}
+
+bool Game::getEvent() {
+    if (XPending(gamedisplay_.getDisplay())) {
+        XNextEvent(gamedisplay_.getDisplay(), &event_);
+        printf("Event: %d\n", event_.type);
+
+        return true; 
     }
 
-    int s = DefaultScreen(d);
-    Window w = XCreateSimpleWindow(d, RootWindow(d, s), 0, 0, 100, 100, 1, 
-                                    BlackPixel(d, s), WhitePixel(d, s));
-    XMapWindow(d, w);
+    return false;
+}
 
-    XPending(d);
+void Game::handleEvent() {
+    if(event_.type == KeyPress) {
+        printf("KeyPress Event: %d\n", event_.xkey.keycode);
+    
+        switch (event_.xkey.keycode) {
+            case KEY_UP       : printf("KEY_UP\n"); break;
+            case KEY_DOWN     : printf("KEY_DOWN\n"); break;
+            case KEY_LEFT     : printf("KEY_LEFT\n"); break;
+            case KEY_RIGHT    : printf("KEY_RIGHT\n"); break;
+            case KEY_SPACEBAR : printf("KEYY__SPACEBAR\n"); break;
+            case KEY_ESCAPE   : printf("KEY_ESCAPE\n"); 
+                                is_running_ = false; break;
+        }
+    }
+}
 
-    getchar();
+}
+
+int main() {
+    mygame::Game game;
+
+    game.run();              
 
     printf("EXITING\n");
 }
